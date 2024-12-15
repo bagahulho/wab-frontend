1393cfddeed6a42ae07cb69d1d7a5fd0c4e60db3
bagahulho <holuhoev.b@wb.ru>
2024-12-15 23:38:29 +0300

    before review 1

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -3,19 +3,31 @@ import "./MainPage.css"
 import {Button} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import Header from "../../components/Header/Header";
+import {useSelector} from "react-redux";
+import {RootState} from "../../store";
 
 const MainPage: React.FC = () => {
+    const { isModerator } = useSelector((state: RootState) => state.auth);
     return (
         <><Header></Header>
             <div>
                 <main className="main-container mt-4">
                     <h1>Добро пожаловать в систему рассылок</h1>
                     <p>Данный сервис предназначен для отправки общего сообщения нескольким чатам, вместо того, чтобы отправлять каждому по отдельности</p>
-                    <Link to="/chats">
-                        <Button variant="outline-primary" size="lg">
-                            Список чатов
-                        </Button>
-                    </Link>
+                    {isModerator ? (
+                        <Link to="/recipient-chats">
+                            <Button variant="outline-primary" size="lg">
+                                Список чатов
+                            </Button>
+                        </Link>
+                    ) : (
+                        <Link to="/chats">
+                            <Button variant="outline-primary" size="lg">
+                                Список чатов
+                            </Button>
+                        </Link>
+                        )
+                    }
                 </main>
             </div>
         </>
